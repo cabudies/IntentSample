aa313b554536ebb53cede31fc7b0cbdc2b008050
Gurjas Singh <gurjassingh92@gmail.com>
2018-06-28 12:22:09 +0530

    send data from one activity to another

--- app/src/main/java/com/brillicaservices/gurjas/intentsample/MainActivity.java
+++ app/src/main/java/com/brillicaservices/gurjas/intentsample/MainActivity.java
@@ -5,9 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText text;
     Button btn;
 
     @Override
@@ -15,15 +17,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        text = findViewById(R.id.edit_text);
         btn = findViewById(R.id.button);
 
         btn.setOnClickListener(new View.
                 OnClickListener() {
             @Override
             public void onClick(View v) {
+                String name = text.getText().toString();
                 Intent intent = new Intent(
                         MainActivity.this,
                         SecondActivity.class);
+                intent.putExtra("Name", name);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/brillicaservices/gurjas/intentsample/SecondActivity.java
+++ app/src/main/java/com/brillicaservices/gurjas/intentsample/SecondActivity.java
@@ -1,8 +1,10 @@
 package com.brillicaservices.gurjas.intentsample;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_activity);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("Name");
+
+        TextView textView;
+        textView = findViewById(R.id.text_view);
+
+        textView.setText("Hello " + name);
     }
 }
